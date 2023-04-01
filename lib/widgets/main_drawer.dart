1ed5be886b762bf457ced7ae796ccf5f61c5ad9d
gj3017 <gj3017@163.com>
2023-04-01 21:40:52 +0800

    nav-14-finished

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -31,7 +31,7 @@ class MainDrawer extends StatelessWidget {
             width: double.infinity,
             padding: EdgeInsets.all(20),
             alignment: Alignment.centerLeft,
-            color: Theme.of(context).colorScheme.secondary,
+            color: Theme.of(context).accentColor,
             child: Text(
               'Cooking Up!',
               style: TextStyle(
